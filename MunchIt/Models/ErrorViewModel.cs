afb4c8b6cddc546a4910a716ed247f5edeb2ceb2
User <User@DESKTOP-ENV0JT7>
2023-12-18 16:38:49 +0300

    Add project files.

---
+++ MunchIt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MunchIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
